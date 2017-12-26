6cf14a431fa7dfdef3c2763a84244f9c41422571
kflange <zknsftk1msrk@gmail.com>
2017-12-26 16:33:28 +0900

    add for-loop

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	// inn-out
 	fmt.Print("Enter a number: ")
 	var inn float64
 	fmt.Scanf("%f", &inn)
 	out := inn * 2
 	fmt.Println(inn, " * 2 = ", out)
+
+	// for-loop
+	for i := 1; i <= 10; i++ {
+		if i % 2 == 0 {
+			fmt.Println(i, "even")
+		} else {
+			fmt.Println(i, "odd")
+		}
+	}
 }
